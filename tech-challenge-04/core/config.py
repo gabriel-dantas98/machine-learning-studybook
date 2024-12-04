9dce5092134e139e3d02f64c8876539def79f16d
gabriel-dantas98 <gabriel@kickops.com.br>
2024-12-03 22:19:33 -0300

    improve observability things

--- tech-challenge-04/core/config.py
+++ tech-challenge-04/core/config.py
@@ -1,11 +1,10 @@
 import os
 
-PROJECT_NAME = "API LST Finance Predict"
-TABLE_NAME = os.environ.get("TABLE_NAME") if os.environ.get("TABLE_NAME") else 'lst-finance'
+PROJECT_NAME = "API LSTM Finance Predict"
 MLFLOW_SERVER_URL = os.environ.get("MLFLOW_SERVER_URL", "http://localhost:5000")
 
 # OpenTelemetry Configuration for Local Development
-GRAFANA_OTLP_ENDPOINT = os.environ.get("GRAFANA_OTLP_ENDPOINT", "http://localhost:14268")
+GRAFANA_OTLP_ENDPOINT = os.environ.get("GRAFANA_OTLP_ENDPOINT", "http://localhost:4318")  # OTLP HTTP endpoint
 GRAFANA_INSTANCE_ID = os.environ.get("GRAFANA_INSTANCE_ID", "local-development")
 GRAFANA_API_KEY = os.environ.get("GRAFANA_API_KEY", "local-key")
 
