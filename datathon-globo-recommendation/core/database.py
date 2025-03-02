ddcab748dfc32f857a104aeaa656f7aee1729eaa
gabriel-dantas98 <gabriel.dantas@quintoandar.com.br>
2025-03-02 15:08:05 -0300

    add training data

--- datathon-globo-recommendation/core/database.py
+++ datathon-globo-recommendation/core/database.py
@@ -7,23 +7,8 @@ from google.cloud.sql.connector import Connector
 
 from core.config import TABLE_NAME
 
-CONNECTION_CLOUD_SQL = sqlalchemy.engine.url.URL.create(
-    drivername="postgresql+pg8000",
-    username="datathon-globo-db",
-    password=os.environ.get("DB_PASSWORD"),
-    database="datathon",
-    query={"host": "/cloudsql/gabsdevops:us-central1:datathon-globo-db"},
-)
-SQLALCHEMY_DATABASE_URL = (
-    CONNECTION_CLOUD_SQL
-    if os.environ.get("DB_URL")
-    else f"postgresql://datathon:datathon@localhost:6025/{TABLE_NAME}"
-)
-
-print("DB_URL LOADED", SQLALCHEMY_DATABASE_URL)
-connector = Connector()
-
 def get_connection():
+    connector = Connector()
     return connector.connect(
         instance_connection_string="gabsdevops:us-central1:datathon-globo-db",  # Nome da conexão da instância
         driver="pg8000",
@@ -32,11 +17,13 @@ def get_connection():
         db="postgres"
     )
 
-# engine = create_engine(SQLALCHEMY_DATABASE_URL)
-engine = sqlalchemy.create_engine(
-    "postgresql+pg8000://",
-    creator=get_connection,
-)
+if os.environ.get("ENVIRONMENT") == 'production':
+    engine = sqlalchemy.create_engine(
+        "postgresql+pg8000://",
+        creator=get_connection,
+    )
+else:
+    engine = create_engine(f"postgresql://datathon:datathon@localhost:6025/{TABLE_NAME}")
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
